c31408b2ed6cb4867c45ef74352963382059d448
unknown <franclase1015@gmail.com>
2026-03-12 20:52:47 +0100

    feat: plantilla base para el index, nav, header y Top
    feat: vistas faltantes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <router-view />
+  <div id="app">
+    <TopBar/>
+    <Header/>
+    <Navbar/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
+import TopBar from "@/components/layout/TopBar.vue"
+import Header from "@/components/layout/Header.vue"
+import Navbar from "@/components/layout/Navbar.vue"
+
 export default {
-  name: "App"
+  name: "App",
+  components: { TopBar, Header, Navbar }
 }
 </script>
+
+<style>
+#app {
+  display: grid;
+  grid-template-rows: auto auto auto 1fr;
+  min-height: 100vh;
+}
+
+#app > router-view {
+  padding: 20px;
+}
+</style>
--- frontend/src/views/InicioView.vue
+++ frontend/src/views/InicioView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="container mt-5">
-    <h1>Inicio</h1>
-  </div>
+    <main class="contenido">
+      <h1>Contenido aquí</h1>
+    </main>
 </template>
+
+<script>
+import TopBar from "@/components/layout/TopBar.vue"
+import Header from "@/components/layout/Header.vue"
+import Navbar from "@/components/layout/Navbar.vue"
+
+export default {
+  name:"InicioView",
+  components:{
+    TopBar,
+    Header,
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+.pagina{
+  display:grid;
+  grid-template-rows: auto auto auto 1fr;
+  min-height:100vh;
+}
+
+.contenido{
+  padding:40px;
+  text-align:center;
+}
+</style>
